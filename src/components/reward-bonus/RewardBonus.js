e36f2232225437799f2629c40efb1b3545e61e65
Noel <chaoticforbidden@gmail.com>
2021-03-26 10:15:56 -0500

    added data array object created RewardBonus

---
+++ src/components/reward-bonus/RewardBonus.js
@@ -0,0 +1,12 @@
+const RewardBonus = (props) => {
+  const receipts = props.data;
+  console.log(receipts);
+  return (
+    <>
+      {" "}
+      <div>Hi</div>{" "}
+    </>
+  );
+};
+
+export default RewardBonus;
