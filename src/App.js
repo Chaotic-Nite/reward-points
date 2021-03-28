01402356bb29dabda095b7115c35c2a3953ea04f
Noel <chaoticforbidden@gmail.com>
2021-03-27 21:28:15 -0500

    commit before pull

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { useState } from "react";
 import { Card } from "react-bootstrap";
+import MonthlyCheck from "./components/monthly-check/MonthlyCheck";
 import RewardBonus from "./components/reward-bonus/RewardBonus";
 
 function App() {
-  const [state, setState] = useState('');
-  const [value, setValue] = useState([])
+  const [state, setState] = useState("");
+  const [value, setValue] = useState([]);
   const [input, setInput] = useState("");
-  const data = require("./test-receipts/data.json");
-  
+  const data = require("./test-receipts/receipts.json");
+  const CURRENT_MONTH = 6;
+
   const searchAccounts = (event) => {
     if (event.key === "Enter") {
       const inputValue = input;
       setInput("");
       const filterdData = data.filter(
         (item) => item.rewardAccount === inputValue
-        );
-        console.log(filterdData)
+      );
+      console.log(filterdData);
       if (filterdData.length === 0) {
         setState("");
         return;
       }
       setState(inputValue);
-      setValue(filterdData)
+      setValue(filterdData);
     }
   };
-  
+
   return (
     <div className="App">
-        <input
-          className="new-todo"
-          placeholder="Enter Rewards Account"
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-          onKeyDown={(event) => searchAccounts(event)}
-          autoFocus
+      <input
+        className="new-todo"
+        placeholder="Enter Rewards Account"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        onKeyDown={(event) => searchAccounts(event)}
+        autoFocus
       />
       <div className="App-header">
-        <Card style={{ width: '25rem' }} bg='primary' >
-        <Card.Header>{state ? `Rewards Account: ${state}` : 'No Rewards Found' }</Card.Header>
+        <Card style={{ width: "25rem" }} bg="primary">
+          <Card.Header>
+            {state ? `Rewards Account: ${state}` : "No Rewards Found"}
+          </Card.Header>
           <Card.Body>
             {value.length > 0 ? <RewardBonus data={value} /> : null}
           </Card.Body>
         </Card>
       </div>
+      />
+      <MonthlyCheck data={data} currentMonth={CURRENT_MONTH} />
     </div>
   );
 }
---
+++ src/components/monthly-check/MonthlyCheck.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MonthlyCheck(props) {
+  return <> </>;
+}
+
+export default MonthlyCheck;
